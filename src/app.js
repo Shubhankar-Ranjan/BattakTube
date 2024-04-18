6ca2f16d820531e6314116c0accb10f357b0f999
Shubhankar_Ranjan <shubhankar515@gmail.com>
2024-04-19 01:23:18 +0530

    added utils code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+// app.use(cors()) // this will allow all the requests from all the origins
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // CORS_ORIGIN = * because we want to allow all the requests from all the origins for now 
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" })) // limit the request body size from json files to 16kb 
+app.use(express.urlencoded({ extended: true, limit: "16kb" })) // limit the request body size from url to 16kb
+app.use(express.static("public")) // public folder is accessible to the public where we can store images or assets
+app.use(cookieParser()) // to parse the cookies from the request
+
+export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// Another method to write the asyncHandler
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+
+// Steps to understand the above code:
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => () => {} // removing the {} from the arrow function
+// const asyncHandler = (func) => async () => {} // adding async
